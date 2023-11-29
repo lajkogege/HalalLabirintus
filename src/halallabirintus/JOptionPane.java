dee5e401009738ac7611c737115eca96817878bd
lajkogege <lajkogege@gmail.com>
2023-11-29 16:08:34 +0100

    design felület

---
+++ src/halallabirintus/JOptionPane.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package halallabirintus;
+
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JRootPane;
+
+/**
+ *
+ * @author LajkóGergő(SZOFT_202
+ */
+class JOptionPane {
+
+    static void showMessageDialog(Object object, ImageIcon kep) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void showConfirmDialog(JRootPane rootPane, ActionEvent evt, String title, int WIDTH, int HEIGHT) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
